d149d215c354516e3e78bfb17e2af823174d837c
Noe <noe.joaquin.james@gmail.com>
2020-01-28 19:56:56 -0500

    spec out last transaction util

--- ruby_tests/jobcoin_spec.rb
+++ ruby_tests/jobcoin_spec.rb
@@ -12,7 +12,7 @@ class TestUtils < Minitest::Test
   def test_get_address_balance
     mock = Minitest::Mock
     def mock.body
-      body = {"balance":"27"}.to_json
+      {"balance":"27"}.to_json
     end
     HTTParty.stub :get, mock do
       assert_equal "27", get_address_balance("address")
@@ -22,7 +22,7 @@ class TestUtils < Minitest::Test
   def test_get_address_transactions
     mock = Minitest::Mock
     def mock.body
-      body = {
+      {
         "transactions": [
           {
             "timestamp":"fake_date",
@@ -42,10 +42,26 @@ class TestUtils < Minitest::Test
     end
   end
 
+  def test_last_transaction_for
+    mock = Minitest::Mock
+    def mock.body
+      {
+        "transactions": [
+          {"transaction1" => 2}
+        ]
+      }.to_json
+    end
+
+    HTTParty.stub :get, mock do
+      transaction = last_transaction_for("address")
+      assert_equal 1, transaction.length
+    end
+  end
+
   def test_get_all_transactions
     mock = Minitest::Mock
     def mock.body
-      body = {
+      {
         "transactions": [
           {"transaction1":"boom"},
           {"transaction2":"chicka"},
