0a2f658e27be5db50330a3ff1bbea294dba5f833
Noe <noe.joaquin.james@gmail.com>
2020-01-30 17:43:08 -0500

    add distribute method

--- index.rb
+++ index.rb
@@ -34,7 +34,7 @@ def prompt(mixer = nil)
      last_trans = last_transaction_for(from_address) #returns hash
      timestamp = last_trans["timestamp"]
      user_addresses = addresses_for_withdrawal.split(",")
-     mixer = JobCoinMixer.new
+     mixer = JobCoinMixer.new unless mixer
      mixer.add_transaction({new_address_for_deposit => [timestamp, user_addresses]})
      puts "Your transaction has been made. Our mixer will now listen, and transfer
      your coins"
@@ -45,10 +45,16 @@ def prompt(mixer = nil)
      puts "mixer internal house: #{mixer.house}"
      puts "mixer deposits to check and move: #{mixer.deposits_to_check_and_move}"
 
+     puts "Are there any more deposits you would like to make? (y/n)"
+     more_deposits = gets.strip
+     if more_deposits == "y"
+       prompt(mixer)
+     else
+       puts "Okay, since there are no other deposits you would like to make,
+       we will now distribute your funds to the addresses you initially provided for withdrawal"
+       mixer.distribute_for_withdrawal
 
-     puts "We will now doll out your tranferred coin to the addresses you provided us"
-
-
+     end
       # puts "addresses_for_withdrawal: #{user_addresses}"
       # puts "mixer_transactions: #{mixer.deposit_address_transactions}"
      # puts "The mixer is currently mixing!\n"
--- jobcoin_mixer.rb
+++ jobcoin_mixer.rb
@@ -3,6 +3,7 @@ require_relative 'utils'
 class JobCoinMixer
   attr_reader :deposits_to_check_and_move, :house
   HOUSE_ADDRESS = "HouseTest".freeze
+  PERCENTAGE_TO_SEND = 0.40
 
   def initialize
     @deposits_to_check_and_move = {}
@@ -28,7 +29,31 @@ class JobCoinMixer
     end
   end
 
+  def distribute_for_withdrawal # test this out
+    zero_amount_count = 0
+    until zero_amount_count == @house.length
+      @house.each do |accounts, amount|
+        if amount < 1 # transfer remainder to whichever account
+          transfer_funds(from: HOUSE_ADDRESS, to: accounts.first, amt: amount)
+          @house[accounts] = "0"
+          zero_amount_count += 1
+        else
+          remaining_amount = distribute_percentage(accounts, amt)
+          @house[accounts] = remaining_amount
+        end
+      end
+    end
+  end
+
   private
+
+  def distribute_percentage(accounts, amt) # dolls out fixed % of remaining amount to random account
+      random_idx = rand(0...accounts.length)
+      addrs_to_send = accounts[random_idx]
+      amt_to_send = amt.to_f * PERCENTAGE_TO_SEND
+      transfer_funds(from: HOUSE_ADDRESS, to: addrs_to_send, amt: amt_to_send.to_s)
+      (amt - amt_to_send).to_s
+  end
 
   def remove_address(address)
     @deposits_to_check_and_move.delete(address)
