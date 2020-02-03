79734b86d92f5e9ced7ffc5e2b25fee472fbb4af
Noe <noe.joaquin.james@gmail.com>
2020-02-03 14:44:44 -0500

    final edits

--- index.rb
+++ index.rb
@@ -6,7 +6,7 @@ require 'colorize'
 def prompt(mixer = nil)
   puts "Welcome to the Jobcoin Mixer!".blue
   puts "Please enter a comma-separated list of new, unused Jobcoin addresses
-where your mixed Jobcoins will be sent for withdrawal:"
+where your mixed Jobcoins will be sent for withdrawal (ex. someAddress1,someAddress2):"
 
   addresses_for_withdrawal = gets.strip
   new_address_for_deposit = generate_deposit_address
@@ -19,17 +19,17 @@ Would you like to deposit for future withdrawal?(y/n)"
   deposit = gets.strip
 
  if deposit == 'y' || deposit == 'yes'
-   puts "Please enter the address from which you will be depositing your jobcoin:"
+   puts "Please enter the address from which you will be depositing your jobcoin (ex. NoesAddress1):"
 
    from_address = gets.strip
    from_address_balance = JobcoinClient.get_address_balance(from_address)
 
    puts "The current balance at #{from_address} is: #{from_address_balance}.
-How much do you want to deposit?"
+How much do you want to deposit?(ex. 3)"
 
   deposit_amount = gets.strip
 
-  puts "Are you sure you want to deposit #{deposit_amount} into #{new_address_for_deposit}? (y/n)".red
+  puts "Are you sure you want to deposit #{deposit_amount} Jobcoin(s) into #{new_address_for_deposit}? (y/n)".red
    go_deposit = gets.strip
 
    if go_deposit == "y"
@@ -42,15 +42,15 @@ How much do you want to deposit?"
        user_address: from_address,
        withdrawal_addresses: user_addresses,
        timestamp: timestamp,
-       total_amt: deposit_amount #DO WE STILL WANT THIS?
+       total_amt: deposit_amount
      }
      mixer.add_transaction(new_address_for_deposit, data_for_mixer)
-     puts "Your transaction has been made. Our mixer will now poll, and transfer your coins"
+     puts "Your transaction has been made. Our mixer will now poll, and transfer your coins".blue
      mixer.poll
 
      puts "your coins have made it to the house account!"
      puts "current balance of the house account:"
-     puts "#{JobcoinClient.get_address_balance("HouseTest")}".green
+     puts "#{JobcoinClient.get_address_balance(JobcoinMixer::HOUSE_ADDRESS)}".green
 
      puts "Are there any more deposits you would like to make? (y/n)"
      more_deposits = gets.strip
